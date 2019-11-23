9f8e21f1d329d976ec10bfb868969e30612bc32a
Mikhail Naganov <mnaganov@google.com>
2019-11-22 17:34:45 -0800

    NX659J: Switch to Audio HAL V6
    Switch coral and flame to Audio HAL V6
    
    Bug: 141989952
    Test: atest VtsHalAudioV6_0TargetTest
          atest VtsHalAudioEffectV6_0TargetTest
                      manual audio "smoke" test on coral
    
    Change-Id: If40ae28c224160728634736ee95590a779f905c4

--- device.mk
+++ device.mk
@@ -42,10 +42,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@5.0-impl \
+    android.hardware.audio@6.0-impl \
     android.hardware.audio.common@2.0-util \
     android.hardware.audio.common@5.0-util \
-    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
