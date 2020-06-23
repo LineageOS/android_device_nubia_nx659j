b25efd5b74b9e2392813b636caecd0d0f104803b
Davide Garberi <dade.garberi@gmail.com>
2020-06-23 14:01:23 +0200

    NX659J: Add initial NubiaParts
    * Only provides display refresh rate management for now
    * Thanks to phhusson implementation for one part of the code
    
    Change-Id: I45407b3387b14f568be822576949ce18a08478ed

--- device.mk
+++ device.mk
@@ -67,6 +67,10 @@ PRODUCT_PACKAGES += \
     FrameworksResCommon \
     FrameworksResTarget
 
+# Parts
+PRODUCT_PACKAGES += \
+    NubiaParts
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.ims.xml
