26fb23ad4d8768c6b7272dbebb6b35e7a0178832
Davide Garberi <dade.garberi@gmail.com>
2020-07-13 00:18:00 +0200

    NX659J: Add stock RIL vendor props
    Change-Id: I73022dd95647f50a432adc1650b912a3bbaa4dae

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -5,3 +5,17 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.use_color_management=true \
     ro.surface_flinger.wcg_composition_dataspace=143261696 \
     ro.surface_flinger.protected_contents=true
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.radio.primarycard=true \
+    ro.com.android.mobiledata=true \
+    ro.com.android.dataroaming=false \
+    persist.vendor.radio.custom_ecc=0 \
+    persist.vendor.radio.apm_sim_not_pwdn=1 \
+    persist.vendor.radio.enableadvancedscan=true \
+    persist.vendor.radio.sib16_support=1 \
+    vendor.rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so \
+    persist.vendor.radio.procedure_bytes=SKIP \
+    persist.radio.multisim.config=dsds \
+    persist.vendor.radio.rat_on=combine
