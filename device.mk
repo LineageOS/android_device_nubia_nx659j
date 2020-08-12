bc7aef69ef10b984b1e700a615c47a8831da1b01
Lucchetto <lucchetto.tie@live.com>
2020-08-12 14:14:36 +0200

    nx659j: Build dummy libqti-perfd-client
    Change-Id: I796e7c1da7eb442d60de7a0183205c049d89149e

--- device.mk
+++ device.mk
@@ -160,7 +160,8 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/lineage/interfaces/power-libperfmgr
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/qcom-caf/common/libqti-perfd-client
 
 # NFC
 PRODUCT_PACKAGES += \
@@ -244,7 +245,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.lineage-libperfmgr
+    android.hardware.power-service.lineage-libperfmgr \
+    libqti-perfd-client
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
--- extract-files.py
+++ extract-files.py
@@ -18,6 +18,7 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
+    'hardware/qcom-caf/common/libqti-perfd-client',
     'hardware/qcom-caf/sm8250',
     'hardware/qcom-caf/wlan',
     'hardware/nubia',
