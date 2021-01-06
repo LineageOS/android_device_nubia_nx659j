a3e95a015dffb0a127cdff49a693e3a89179f9e2
LuK1337 <priv.luk@gmail.com>
2021-01-06 19:30:19 +0100

    NX659J: Build vndservicemanager
    * Needed for FCM >= R.
    
    Change-Id: Ibc27a98f5774a51caaab077c1d8e0a2a79bdffe5

--- device.mk
+++ device.mk
@@ -377,6 +377,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.1-service-qti
 
+# Vendor service manager
+PRODUCT_PACKAGES += \
+    vndservicemanager
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-service.NX659J
