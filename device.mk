5879fdc4de6bbe607606bc2566da8fb04aba16c5
Davide Garberi <dade.garberi@gmail.com>
2021-03-12 10:28:08 +0100

    NX659J: fod: Rename the service
    * Match it with all other custom services
    
    Change-Id: Ida89bbc250bad6969b1d71f8c1da5db8db8139c2

--- device.mk
+++ device.mk
@@ -153,7 +153,7 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service \
-    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.nubia_NX659J
+    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.NX659J
 
 # Fstab
 PRODUCT_COPY_FILES += \
