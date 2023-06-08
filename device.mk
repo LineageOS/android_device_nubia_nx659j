35dd0baa4c78d1e28aaa919b6c4659146d75023f
Davide Garberi <dade.garberi@gmail.com>
2023-06-08 18:46:23 +0200

    NX659J: Switch to commonized libhidl shims
    Change-Id: I499342d47ebb54304a1b3558ffb7c7f8ced0a0d9

--- device.mk
+++ device.mk
@@ -165,7 +165,7 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0_system
+    android.hidl.base@1.0.vendor
 
 # IPACM
 PRODUCT_PACKAGES += \
