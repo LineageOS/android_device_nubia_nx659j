c01d8d1f16bcc85ca931567c75049cebc646a3f5
Davide Garberi <dade.garberi@gmail.com>
2023-06-09 16:10:21 +0200

    NX659J: Update system sepolicy dir inclusions
    Change-Id: I98617665aa52fcc56b45709b83c585e67a738eed

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -156,8 +156,8 @@ VENDOR_SECURITY_PATCH := 2020-06-05
 # Sepolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # VNDK
