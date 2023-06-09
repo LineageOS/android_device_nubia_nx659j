74e53d4fae9c0729f72ce457087900ecc5044707
Davide Garberi <dade.garberi@gmail.com>
2023-06-09 02:53:55 +0200

    NX659J: Update drm entries to 1.4
    Change-Id: Icb46e2aa511c28327b23a67f4e2c02a5cbc3f021

--- device.mk
+++ device.mk
@@ -136,8 +136,8 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.3.vendor \
+    android.hardware.drm@1.4.vendor \
     android.hardware.drm@1.3-service.clearkey
 
 # fastbootd
