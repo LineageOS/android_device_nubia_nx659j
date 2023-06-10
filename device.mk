e8786484b538859c2597afd1cb0e74ed094a32ad
Davide Garberi <dade.garberi@gmail.com>
2023-06-10 15:16:23 +0200

    NX659J: Remove custom lights hal
     * Needs to be re-done in AIDL, also this HAL didn't work properly
       to begin with
    
    Change-Id: Ibbb6ab7e6c5dae380aa64bc16a09bd8f98633fcd

--- device.mk
+++ device.mk
@@ -200,10 +200,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
-# Lights
-PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.NX659J
-
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
