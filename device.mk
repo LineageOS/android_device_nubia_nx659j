707dd58c69f41b7488c7100941dc6345674db431
Davide Garberi <dade.garberi@gmail.com>
2023-06-14 01:41:24 +0200

    NX659J: Import WifiResCommon and WifiResTarget from stock
    * Unified in a single overlay
    
    Change-Id: Ifa9c9491b99682296afd422d7d337a5a8e28a8e4

--- device.mk
+++ device.mk
@@ -432,6 +432,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.fstman@1.0.vendor \
     vendor.qti.hardware.wifi.hostapd@1.1.vendor \
     vendor.qti.hardware.wifi.supplicant@2.1.vendor \
+    WifiResCommon \
     wpa_supplicant \
     wpa_supplicant.conf
 
