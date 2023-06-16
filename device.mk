b00321db1c3201f9dbf85390882920b73dc0b8a4
Davide Garberi <dade.garberi@gmail.com>
2023-06-17 00:35:01 +0200

    NX659J: Switch to multihal sensors service
    Change-Id: I69865d5aa76937bed8b86a8927aa4cbbabc17095

--- device.mk
+++ device.mk
@@ -384,10 +384,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service.NX659J \
-    android.hardware.sensors@2.0.vendor \
-    android.hardware.sensors@2.0-ScopedWakelock.vendor
+    android.hardware.sensors@2.0-service.multihal
 
 # Telephony
 PRODUCT_PACKAGES += \
