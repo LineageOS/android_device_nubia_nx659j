7d2b36cbe18df111788b804fb64ebecbf54d5368
Davide Garberi <dade.garberi@gmail.com>
2023-06-17 00:42:11 +0200

    NX659J: Switch perf vendor libs to source built
    Change-Id: I8a750ba50954943080e604092623fa0b7819155f

--- device.mk
+++ device.mk
@@ -337,7 +337,8 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    android.hardware.power@1.2.vendor
+    android.hardware.power@1.2.vendor \
+    vendor.qti.hardware.perf@2.2.vendor
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
