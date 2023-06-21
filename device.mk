ef61ac48653d64d7658150d0b19dfb7d6e3924f1
Davide Garberi <dade.garberi@gmail.com>
2023-06-21 02:15:20 +0200

    NX659J: Switch to stock camera.device libs
     * Apparently Nubia made some changes on these libs, and without using
       stock ones, video recording is completely broken.
     * Switch to stock libs for now, in the future trying to reverse the
       changes might be a good idea
    
    Change-Id: I3b1d08cc7c7565008e264a3efccbbf0d3ee0fc75

--- device.mk
+++ device.mk
@@ -95,7 +95,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     android.frameworks.sensorservice@1.0.vendor \
-    camera.device@3.5-impl \
     libcamera2ndk_vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
