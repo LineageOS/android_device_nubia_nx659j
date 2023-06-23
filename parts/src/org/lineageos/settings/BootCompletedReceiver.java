33afd67919bd913171578b230beddac2e88f7662
Davide Garberi <dade.garberi@gmail.com>
2023-06-24 00:37:03 +0200

    NX659J: parts: Update BootCompletedReceiver for latest Fan rework
    Change-Id: I35c4f49102078f51e84dc3f64768100f21f6410b

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -20,6 +20,8 @@ package org.lineageos.settings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import org.lineageos.settings.fan.FanFragment;
@@ -34,11 +36,16 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        int intValue = Integer.parseInt(sharedPreferences.getString(FanFragment.KEY_FAN_MODE, String.valueOf(FanFragment.FAN_AUTO_VALUE)));
+
         if (SettingsUtils.getEnabled(context, FanFragment.KEY_FAN_ENABLE)) {
-            if (SettingsUtils.getEnabled(context, FanFragment.KEY_FAN_AUTO))
+            if (intValue == FanFragment.FAN_AUTO_VALUE) {
                 FileUtils.writeLine(FanFragment.SMART_FAN, "1");
-            else if (SettingsUtils.getEnabled(context, FanFragment.KEY_FAN_MAX))
-                FileUtils.writeLine(FanFragment.SPEED_LEVEL, "5");
+            } else if (intValue == FanFragment.FAN_MANUAL_VALUE) {
+                String fanSpeed = String.valueOf(SettingsUtils.getInt(context, FanFragment.KEY_FAN_MANUAL, 1));
+                FileUtils.writeLine(FanFragment.SPEED_LEVEL, fanSpeed);
+            }
         }
     }
 }
