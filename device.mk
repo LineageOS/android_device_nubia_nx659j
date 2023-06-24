4b51afa6a3c271b844c4b23c23f7aa5799e1b158
Davide Garberi <dade.garberi@gmail.com>
2023-06-24 11:50:09 +0200

    NX659J: Move powerhint to device tree
     * Since Nubia's powerhint is outdated, this allows updating it
       without having to take it completely from other OEMs, which
       would mean having their proprietary bits as well.
    
    Change-Id: Ib8ae76c059d25ec23d419412f9fe348d83acbced

--- device.mk
+++ device.mk
@@ -333,6 +333,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
     vendor.qti.hardware.perf@2.2.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
