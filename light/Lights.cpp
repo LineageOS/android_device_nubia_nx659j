c2fb1e489be7f8894ba985523a451ef81a8d6422
Davide Garberi <dade.garberi@gmail.com>
2023-06-28 18:33:04 +0200

    NX659J: light: Add files utilies functions
    Change-Id: Ie3825d6aba0b27cecf3528e0d5ed6c375bbdabdf

--- light/Lights.cpp
+++ light/Lights.cpp
@@ -7,6 +7,56 @@
 #include "Lights.h"
 
 #include <android-base/logging.h>
+#include <fstream>
+
+namespace {
+/*
+ * Write value to path and close file.
+ */
+static void set(std::string path, std::string value) {
+    std::ofstream file(path);
+
+    if (!file.is_open()) {
+        LOG(WARNING) << "failed to write " << value.c_str() << " to " << path.c_str();
+        return;
+    }
+
+    file << value;
+}
+
+static int get(std::string path) {
+    std::ifstream file(path);
+    int value;
+
+    if (!file.is_open()) {
+        LOG(WARNING) << "failed to read from: " << path.c_str();
+        return 0;
+    }
+
+    file >> value;
+    return value;
+}
+
+static int readStr(std::string path, char *buffer, size_t size)
+{
+
+    std::ifstream file(path);
+
+    if (!file.is_open()) {
+        LOG(WARNING) << "failed to read: " << path.c_str();
+        return -1;
+    }
+
+    file.read(buffer, size);
+    file.close();
+    return 1;
+}
+
+static void set(std::string path, int value) {
+    set(path, std::to_string(value));
+}
+
+}  // anonymous namespace
 
 namespace aidl {
 namespace android {
