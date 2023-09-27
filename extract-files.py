2d4db1956f93ddc6238e017ac18b4352511d20b6
Alexander Martinz <amartinz@shiftphones.com>
2023-09-27 17:13:12 +0200

    nx659j: fingerprint: use libhidlbase-v32 for goodix
    Test: run cts-on-gsi -m CtsBiometricsTestCases
    Test: run cts-on-gsi -m CtsHardwareTestCases -t android.hardware.fingerprint.cts.FingerprintManagerTest
    Test: run vts -m VtsHalBiometricsFingerprintV2_1TargetTest
    Test: run vts -m VtsHalBiometricsFingerprintV2_2TargetTest
    Change-Id: Iaef8ede57250aa171f9863e7e250c604ef241334
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- extract-files.py
+++ extract-files.py
@@ -46,7 +46,10 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     ('vendor/etc/sensors/hals.conf'): blob_fixup()
         .regex_replace('ffbm.sensors.oem.so\n', ''),
-    ('vendor/lib/hw/fingerprint.goodix_fod.default.so', 'vendor/lib64/hw/fingerprint.goodix_fod.default.so'): blob_fixup()
+    ('vendor/lib/hw/fingerprint.goodix_fod.default.so', 
+    'vendor/lib64/hw/fingerprint.goodix_fod.default.so',
+    'vendor/lib/libvendor.goodix.hardware.biometrics.fingerprint@2.1.so',
+    'vendor/lib64/libvendor.goodix.hardware.biometrics.fingerprint@2.1.so'): blob_fixup()
         .fix_soname()
         .replace_needed('libhidlbase.so', 'libhidlbase-v32.so'),
     'vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so': blob_fixup()
