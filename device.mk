5becce6c7d1d64e8f528bc2ab8ddb80cc8cd0f3e
LuK1337 <priv.luk@gmail.com>
2023-10-20 00:53:15 +0200

    nx659j: Build libprotobuf-cpp-lite-3.9.1-vendorcompat
    Change-Id: I0aac742db9c2b4b3e48ee20158f19405863893c2

--- device.mk
+++ device.mk
@@ -345,7 +345,8 @@ PRODUCT_COPY_FILES += \
 
 # Protobuf
 PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-3.9.1-vendorcompat
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
