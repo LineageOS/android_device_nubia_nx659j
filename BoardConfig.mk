27920abd86eec5251d3bc9f5ee96881203ea9e0a
Bruno Martins <bgcngm@gmail.com>
2023-10-24 18:12:15 +0100

    nx659j: Migrate to restructured sepolicy_vndr
    The new folder structure now looks like this:
    
      device/qcom/sepolicy_vndr
      ├─ legacy-um (meant for sdm845 until sm8350)
      ├─ sm8450
      └─ sm8550
    
    SEPolicy.mk placed in the root automatically redirects devices to the
    proper subfolder.
    
    Change-Id: I4ed1a06af779909d64172da294c4a99a3ae9ba0d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -198,7 +198,7 @@ TARGET_SCREEN_DENSITY := 420
 VENDOR_SECURITY_PATCH := 2022-08-01
 
 # Sepolicy
-include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
+include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
