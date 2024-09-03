6b813062659a7240c32f0c39633e66df341b06fe
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-09-03 02:29:03 +0200

    nx659j: Drop media_profiles_vendor.xml
    Same as media_profiles_kona.xml
    
    Change-Id: I9d22a09c0bc9cf653f0d3f68f02807c893f447bd

--- device.mk
+++ device.mk
@@ -205,7 +205,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml \
     $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/media/media_profiles_kona.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_kona.xml \
-    $(LOCAL_PATH)/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml \
     $(LOCAL_PATH)/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml
 
 # Net
