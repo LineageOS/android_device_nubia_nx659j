f7920695755f2f967d94bad8c6d012377bd2b5b7
Michael Bestas <mkbestas@lineageos.org>
2024-09-26 01:39:46 +0300

    nx659j: Switch to AIDL USB HALs
    Change-Id: I70f6a4e57eef577d86987bfae44f381a4f107624

--- device.mk
+++ device.mk
@@ -385,7 +385,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb-service.qti
 
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
