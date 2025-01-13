92b24737e313bcffac5cdac59c6324257c996c03
Michael Bestas <mkbestas@lineageos.org>
2025-01-13 11:03:38 +0200

    nx659j: Migrate mount point creation out of Android.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b

--- device.mk
+++ device.mk
@@ -257,6 +257,11 @@ PRODUCT_PACKAGES += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 # Parts
 PRODUCT_PACKAGES += \
     NubiaParts
