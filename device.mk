48d374b23e23fb20c7210ccbfa903a1ab39c8d9f
Bruno Martins <bgcngm@gmail.com>
2025-02-06 21:32:31 +0000

    nx659j: Use aidl sensors multihal
    Change-Id: I21da3da39e9726dca693d6bcbe42e93be861f7c7

--- device.mk
+++ device.mk
@@ -358,7 +358,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal
+    android.hardware.sensors-service.multihal
 
 # Telephony
 PRODUCT_PACKAGES += \
