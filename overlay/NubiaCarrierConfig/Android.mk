4deb2d5cfe52b2646e6902a7791c52fc8c931bfa
Wiktor Rudzki <zlewchan@outlook.com>
2025-07-01 00:07:19 +0200

    nx659j: overlay: Android.mk -> Android.bp
    Change-Id: I289d6f1dfcefb55514869a5e744ba6d38a61bc13

--- overlay/NubiaCarrierConfig/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_AAPT_FLAGS := --keep-raw-values
-LOCAL_PACKAGE_NAME := NubiaCarrierConfig
-LOCAL_SDK_VERSION := current
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_RRO_PACKAGE)
--- overlay/NubiaSettings/Android.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_PACKAGE_NAME := NubiaSettings
-LOCAL_SDK_VERSION := current
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_RRO_PACKAGE)
