8da37ebf652d66fa8e335983190af4749269db2f
Wiktor Rudzki <zlewchan@outlook.com>
2025-07-13 02:07:06 +0200

    nx659j: Drop sec_config from device tree
    We have the same unmodified file in vendor tree
    
    Change-Id: I2c0d2092c31d7929c50649bd527e1b37e2c6db53

--- device.mk
+++ device.mk
@@ -180,10 +180,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
-# IRSC
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
-
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
