0de239e0b6c5c09ec460ebce7e108495b944dda0
Wiktor Rudzki <zlewchan@outlook.com>
2026-08-20 22:43:21 +0200

    nx659j: Remove media_codecs.xml
    Should be removed with OMX
    
    Change-Id: I092bd7c01b513946cdb5985dc66780327dd5ddb9

--- device.mk
+++ device.mk
@@ -146,7 +146,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
     $(LOCAL_PATH)/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml \
-    $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/media/media_profiles_kona.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
     $(LOCAL_PATH)/media/video_system_specs.json:$(TARGET_COPY_OUT_VENDOR)/etc/video_system_specs.json
 
