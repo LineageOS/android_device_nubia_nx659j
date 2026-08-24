beac88d169952876f7d94541c0c3f829cbc131fa
Wiktor Rudzki <zlewchan@outlook.com>
2026-08-24 16:55:10 +0200

    nx659j: Properly separate Media section
    Change-Id: Ie27dd608a0c33bcd3f7992a0c59ca4f5da6b02bd

--- device.mk
+++ device.mk
@@ -143,6 +143,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay-service.nx659j
 
+# Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
     $(LOCAL_PATH)/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml \
